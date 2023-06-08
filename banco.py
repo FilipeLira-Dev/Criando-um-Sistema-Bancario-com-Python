cbc2fde6ea6046e6400013ee8eb83a65ed886f44
Filipe Lira <filipelira.dev@gmail.com>
2023-06-08 19:37:57 -0300

    inclusão de limite de saque em R00.00

--- banco.py
+++ banco.py
@@ -1,35 +1,38 @@
-print('#'*10, 'MENU', '#'*10)
-print('\n'
+saldo = 0
+saques_realizados = 0
+max_saque_dia = 3
+limite_saque = 500
+op = 0
+
+while True:
+    print('#'*10, 'MENU', '#'*10)
+    print('\n'
       '[1] Depósito\n'
       '[2] Saque\n'
       '[3] Extrato\n'
       '[4] sair\n')
-print('#'*26)
-
-extrato = int()
-cont_saq = 3
-
-while True:
-    try:
-        op = int(input('Qual a sua opção? '))
-    except ValueError:
-        print('Opção inválida. Tente novamente...')
+    print('#'*26)
+    
+    op = int(input('Qual a sua opção? '))
     
     if op == 1:
         vl_d = float(input('Quanto deseja depositar? R$ '))
-        extrato += vl_d
+        saldo += vl_d
     elif op == 2:
-        vl_s = float(input('Quanto deseja sacar? R$ '))
-        if vl_s > extrato:
-            print('Você não tem saldo suficiente.')
+        if saques_realizados < max_saque_dia:
+            valor_saque = float(input("Digite o valor do saque: "))
+            if valor_saque <= saldo and valor_saque <= limite_saque:
+                saldo -= valor_saque
+                saques_realizados += 1
+                print("Saque realizado com sucesso!")
+            else:
+                print("Valor de saque inválido! Acima do limite diário ou saldo insuficiente.")
         else:
-            extrato -= vl_s
-            cont_saq -= 1
-        if cont_saq < 0:
-            print('Sinto muito, mas você tem um limite diário de apenas três saques por dia e por isso não poderá efetuar mais saques hoje.')
+            print("Limite de saques diários atingido!")
     elif op == 3:
-        print(f'Seu saldo é de R$ {extrato:.2f}')
+        print(f'Seu saldo é de R$ {saldo:.2f}')
     elif op == 4:
+        print("Encerrando o programa...")
         break
     else:
-        print(f'Opção invalida. Tente novamente...')
+        print("Opção inválida! Por favor, escolha uma opção válida.")
